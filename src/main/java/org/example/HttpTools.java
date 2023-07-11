ef4cf8b7b12f6fc55b1c48707453ea7b7a1a18a3
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2023-07-11 14:21:40 +0200

    renamed:    src/main/java/org/example/SecMain.java -> src/main/java/org/example/HttpTools.java 	modified:   .gitignore 	modified:   .idea/workspace.xml 	modified:   src/main/java/org/example/HttpTools.java 	modified:   src/main/java/org/example/Main.java 	.idea/httpRequests/

--- src/main/java/org/example/HttpTools.java
+++ src/main/java/org/example/HttpTools.java
@@ -13,18 +13,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class SecMain {
-    public static void main(String[] args) {
+    private HttpResponse<String> httpResponse;
+    private String url = "http://api.nbp.pl/api/exchangerates/tables/A/";
+
+    public void apiNBP() {
         HttpClient httpClient = HttpClient.newHttpClient();
         try {
-            String url = "http://api.nbp.pl/api/exchangerates/tables/A/";
             HttpRequest request = HttpRequest.newBuilder()
                     .header("Accept", "application/json")
                     .uri(new URI(url))
                     .build();
-
-            HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println(httpResponse.body());
-
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void processResponse(){
             Gson gson = new Gson();
             JsonArray jsonArray = gson.fromJson(httpResponse.body(), JsonArray.class);
             JsonObject jsonObject = jsonArray.get(0).getAsJsonObject();
@@ -34,14 +39,12 @@ public class SecMain {
                 String code = rateObject.get("code").getAsString();
                 double value = rateObject.get("mid").getAsDouble();
             }
-
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+
+    public static void main(String[] args) {
+        SecMain secMain = new SecMain();
+        secMain.apiNBP();
+        secMain.processResponse();
 
     }
 }
